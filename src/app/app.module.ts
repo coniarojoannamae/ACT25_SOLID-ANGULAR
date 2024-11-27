3410e412323f78be2f2423a2cdcde90f3219c824
coniarojoanna <coniarojoannamae093@gmail.com>
2024-11-27 19:05:23 +0800

    #TASK_(SRP)

---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {RouterModule, Routes} from '@angular/router';
+import {FormsModule} from '@angular/forms';
+import {CommonModule} from "@angular/common";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {AppComponent} from "./app.component";
+import { TaskComponent } from './components/task/task.component';
+
+const routes: Routes = [
+    { path: 'task', component: TaskComponent},
+
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes, {enableTracing: true}),
+    FormsModule,
+
+  ],
+
+  declarations: [
+    AppComponent,
+    TaskComponent,
+
+],
+
+
+bootstrap: [
+    AppComponent
+    ]
+  
+  })
+  export class AppModule { }
